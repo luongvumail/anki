7b0e6ba2ced3ceb94b7e281ae1005577d84d2eda
luongvu.mail <luongvu.mail@gmail.com>
2026-08-11 15:05:09 +0700

    chore: remove entire project codebase and configuration files

--- src/domain/deck/deckRepository.i.ts
+++
@@ -1,8 +0,0 @@
-import { DeckEntity } from "./deckEntity.js";
-
-export interface IDeckRepository {
-  getAll(): Promise<DeckEntity[]>;
-  getById(id: string): Promise<DeckEntity | null>;
-  save(deck: DeckEntity): Promise<void>;
-  delete(id: string): Promise<void>;
-}
